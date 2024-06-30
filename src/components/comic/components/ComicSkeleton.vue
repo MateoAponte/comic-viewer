0b8ce7d39dc251b82298509cfa6c05dd696c148d
Mateo Ap <mateo.aponte@masivian.com>
2024-06-30 00:05:01 -0500

    feat: improve skeleton feature

--- src/components/comic/components/ComicSkeleton.vue
+++ src/components/comic/components/ComicSkeleton.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="comic-preview">
     <div class="comic-preview__header">
-      <span class="comic-preview__header-num"></span>
-      <h5 class="comic-preview__header-title"> - </h5>
+      <span class="comic-preview__header-num">#</span>
+      <h5 class="comic-preview__header-title--skeleton"> </h5>
     </div>
     <div class="comic-preview__content">
-      <img class="comic-preview__content-image--loader" :src="Loader" alt="Loader skeleton image" />
-      <span class="comic-preview__content-num">###</span>
-      <span class="comic-preview__content-description"> - </span>
+      <div class="comic-preview__content-image--loader">
+        <grid-loader :loading="trigger" color="#454655" size="60px" margin="20px"></grid-loader>
+      </div>
+      <span class="comic-preview__content-date--skeleton"></span>
+      <span class="comic-preview__content-description--skeleton"> </span>
     </div>
   </div>
 </template>
 
 <script setup>
-import Loader from '../../../assets/image/loader.png'; // => or relative path
+import GridLoader from 'vue-spinner/src/GridLoader.vue';
 </script>
