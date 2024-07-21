0065f3e3293ab6ff9a99457b86b0220127160234
Mathew Ap <mateo.aponte@masiv.com>
2024-07-20 20:53:49 -0500

    feat: Change stlyes to comic layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
 import Comic from './components/comic/Comic.vue';
+import Header from './components/header/Header.vue';
 </script>
 
 <template>
   <section class="comic">
-    <div className="comic-title__header">
-      <h3 class="comic-title">
-        <span class="comic-title--colored">Com</span>ic
-      </h3>
-      <h3 class="comic-title comic-title--moved">
-        <span class="comic-title--colored">Vi</span>ewer
-      </h3>
-    </div>
+    <Header />
     <Comic />
   </section>
 </template>
