bccdb83b0200227c7caf5a56f51a64fa05c17feb
Mathew Ap <mateo.aponte@masiv.com>
2024-07-22 10:44:06 -0500

    feat: Add footer text

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Comic from './components/comic/Comic.vue';
 import Header from './components/header/Header.vue';
+import Footer from './components/footer/Footer.vue';
 </script>
 
 <template>
   <section class="comic">
     <Header />
     <Comic />
+    <Footer />
   </section>
 </template>
---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import { TiOutlineHeartHalf } from '@kalimahapps/vue-icons';
+</script>
+
+<template>
+  <footer class="comic-footer">
+    <span class="comic-footer__text">
+      Front End As a Science <TiOutlineHeartHalf />
+    </span>
+  </footer>
+</template>
